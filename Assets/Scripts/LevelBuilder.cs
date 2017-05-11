b3e7a7d8dead443eed75c7338c35739888a46cf3
kasradzenika <kasradzenika@yahoo.com>
2017-05-11 20:48:03 +0400

    finished creating dynamic platforms creation

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class LevelBuilder : MonoBehaviour
 {
 	public Transform player;
-	public GameObject platform;	//clone this object in every 50 meters
+	public GameObject[] platforms;
 	public int numberOfStartingPlatforms;
-
+	public float destroyTime = 5f; //destroy platforms after some time
 	public Vector3 platformSpawnPoint = Vector3.zero;
 
 	//-------------------- Unity Functions: ------------------------------------
@@ -15,6 +15,7 @@ public class LevelBuilder : MonoBehaviour
 	{
 		int counter = 0;
 
+		//create starting platforms
 		while (counter < numberOfStartingPlatforms)
 		{
 			CreateNextPlatform ();
@@ -22,16 +23,42 @@ public class LevelBuilder : MonoBehaviour
 		}
 	}
 
+	int pp;
+	int lastPP = 0;
+	void Update()
+	{
+		//number of 50 meter segments passed
+		pp = (int) player.position.z / 50;
+
+		//if moving to a new segment
+		if (pp > lastPP) 
+		{
+			//create a new platform
+			CreateNextPlatform ();
+		}
+
+		lastPP = pp;
+	}
+
 	//-------------------- My Custom Functions ------------------------------------
 	void CreateNextPlatform()
 	{
 		Debug.Log ("Creating Platform");
 
+		//move spawn position on z axis
 		platformSpawnPoint.z = platformSpawnPoint.z + 50f;
 
-		GameObject clone;
-		clone = Instantiate (platform);
+		//randomly choose a platfrom
+		int platformChooser = Random.Range(0, platforms.Length);
 
+		//create a new GameObject
+		GameObject clone;
+		clone = Instantiate ( platforms[ platformChooser ] );
+
+		//move the new GameObject to spawn position
 		clone.transform.position = platformSpawnPoint;
+
+		//destroy the object after some time
+		Destroy (clone, destroyTime);
 	}
 }
