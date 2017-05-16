f1cfa349b5b4b30d36d3329a3e9800f9c96cd1e9
kasradzenika <kasradzenika@yahoo.com>
2017-05-16 22:58:48 +0400

    added sounds, fixed jumping, added skybox

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
 	public Rigidbody myBody;
-	public float maxSpeed;
+    public AudioSource sfxAudioPlayer;
+
+    public AudioClip jumpUpSound;
+    public AudioClip jumpDownSound;
+    public AudioClip gameOverSound;
+
+    public float rayCastDistance = 1f;
+    public float maxSpeed;
 	public float moveAcceleration;
 	public float jumpAcceleration;
+    public float gameOverHeight = -5f;
 	private bool isGrounded = false;
+    private bool isGameOver = false;
 
 	void FixedUpdate ()
 	{
+        GroundChecker ();
 		ConstantMove ();
 	}
 
 	void Update()
 	{
-		if (isGrounded && Input.GetKeyDown (KeyCode.Space))
+        //check for game over
+        if (transform.position.y < gameOverHeight && isGameOver == false)
+        {
+            isGameOver = true;
+            sfxAudioPlayer.PlayOneShot(gameOverSound);
+        }
+
+        //jump
+		if (isGrounded && 
+            ( Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown(0) ) )
 		{
 			Jump ();
 		}
-	}
 
+        //visualize raycast in GroundChecker()
+        Debug.DrawLine(transform.position,
+            transform.position + Vector3.down * rayCastDistance);
+    }
+
+    //play jump down sound
 	void OnCollisionEnter(Collision collisionInfo)
 	{
-		isGrounded = true;
-	}
-
-	void OnCollisionExit(Collision collisionInfo)
-	{
-		isGrounded = false;
+        sfxAudioPlayer.PlayOneShot(jumpDownSound);
 	}
 
 	/// <summary>
@@ -55,13 +72,29 @@ public class PlayerController : MonoBehaviour
 		myBody.velocity = newVelocity;
 	}
 
-	/// <summary>
-	/// makes the player jump
-	/// </summary>
-	void Jump()
+    /// <summary>
+    /// Checks if the player is on the ground
+    /// </summary>
+    void GroundChecker()
+    {
+        Ray ray = new Ray();
+
+        ray.origin = transform.position;
+        ray.direction = Vector3.down;
+
+        //launch ray to check if player is grounded
+        isGrounded = Physics.Raycast(ray, rayCastDistance);
+    }
+
+    /// <summary>
+    /// makes the player jump
+    /// </summary>
+    void Jump()
 	{
 		Vector3 jumpVelocity = myBody.velocity;
 		jumpVelocity.y = jumpVelocity.y + jumpAcceleration;
 		myBody.velocity = jumpVelocity;
+
+        sfxAudioPlayer.PlayOneShot(jumpUpSound);
 	}
 }
